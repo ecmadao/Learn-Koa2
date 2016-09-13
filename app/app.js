2d157d4872339869ca9b771eefcfa71891e1a211
ecmadao <wlec@outlook.com>
2016-09-13 23:19:23 +0800

    init koa

---
+++ app/app.js
@@ -0,0 +1,30 @@
+import Koa from 'koa';
+const app = new Koa();
+
+app.use((ctx, next) => {
+  const start = new Date;
+  console.log('app 1')
+  return next().then(() => {
+    const ms = new Date - start;
+    console.log('app 1 callback')
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+  });
+});
+
+app.use(async (ctx, next) => {
+  const start = new Date;
+  console.log('app 2')
+  await next();
+  const ms = new Date - start;
+  console.log('app 2 callback')
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+app.use(ctx => {
+  console.log('app 3')
+  ctx.body = "hello KOA";
+});
+
+app.listen(7000);
+
+export default app;
