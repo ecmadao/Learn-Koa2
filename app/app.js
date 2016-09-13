223e4c127fd7117830c11dc85371b07921e098ac
ecmadao <wlec@outlook.com>
2016-09-14 00:55:58 +0800

    router

--- app/app.js
+++ app/app.js
@@ -1,5 +1,15 @@
 import Koa from 'koa';
+import koaRouter from 'koa-router';
+const router = koaRouter();
 const app = new Koa();
+
+router.get('/about', (ctx, next) => {
+    ctx.body = "this is the about page";
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.use((ctx, next) => {
   const start = new Date;
