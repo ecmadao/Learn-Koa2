c9acaf993e643e37d0c90f6ab98a947342cc3678
ecmadao <wlec@outlook.com>
2016-09-23 09:13:14 +0800

    css/js file

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,15 +1,17 @@
 const homeIndex = async (ctx, next) => {
-  await ctx.render('home/index', {
-    title: 'index page',
-    content: 'index page'
-  });
+  await ctx.redirect('/user');
+  // await ctx.render('home/index', {
+  //   title: 'index page',
+  //   content: 'index page'
+  // });
 };
 
 const about = async (ctx, next) => {
-  await ctx.render('home/about', {
-    title: 'about page',
-    content: 'about page'
-  });
+  await ctx.redirect('/user');
+  // await ctx.render('home/about', {
+  //   title: 'about page',
+  //   content: 'about page'
+  // });
 };
 
 export default {
---
+++ app/frontend/javascripts/entry/user.js
@@ -0,0 +1,1 @@
+import '../../stylesheets/user/index.css';
